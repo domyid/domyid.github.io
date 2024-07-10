132b4e4a348b4fc46d331a79dd856f7f95b225bd
daulJsx <mrdaul09@gmail.com>
2024-07-10 12:57:50 +0700

    Feat: Integrate Google One Tap Authentication and Cookie Storage

--- js/main.js
+++ js/main.js
@@ -14,4 +14,46 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         }
     }
+
+    window.handleCredentialResponse = async (response) => {
+      try {
+        const res = await fetch(
+          "https://asia-southeast2-awangga.cloudfunctions.net/domyid/auth/users",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ token: response.credential }),
+          }
+        );
+
+        const data = await res.json(); // Parse JSON response
+
+        if (res.ok) {
+          // Save token to cookies
+          console.log(data);
+          Cookies.set("login", data.token, {
+            expires: 1, // 1 day
+            sameSite: "Strict",
+            secure: true,
+            path: "/", // Ensure the path is set to root
+          });
+
+        //  Redirect to dashboard
+          window.location.href = "/dashboard"; // Ganti dengan URL dashboard Anda
+        } else {
+          console.error("Login failed:", data.message);
+          alert("Login failed: " + data.message);
+        }
+      } catch (error) {
+        console.error("Error handling credential response:", error);
+        alert(
+          "An error occurred while processing your login. Please try again."
+        );
+      }
+    };
+
 });
+
+
