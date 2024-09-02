fde74a92f6628c93692b510139d52fe475a43b3d
Rolly Maulana Awangga <awangga@gmail.com>
2024-09-02 10:19:02 +0700

    domcontent loaded

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-const menus = document.querySelectorAll('.navbar-burger');
-const dropdowns = document.querySelectorAll('.navbar-menu');
+document.addEventListener('DOMContentLoaded', function() {
+    const menus = document.querySelectorAll('.navbar-burger');
+    const dropdowns = document.querySelectorAll('.navbar-menu');
 
-if (menus.length && dropdowns.length) {
-    for (var i = 0; i < menus.length; i++) {
-        menus[i].addEventListener('click', function() {
-            for (var j = 0; j < dropdowns.length; j++) {
-                dropdowns[j].classList.toggle('is-active');
-            }
-        });
+    if (menus.length && dropdowns.length) {
+        for (var i = 0; i < menus.length; i++) {
+            menus[i].addEventListener('click', function() {
+                for (var j = 0; j < dropdowns.length; j++) {
+                    dropdowns[j].classList.toggle('is-active');
+                }
+            });
+        }
     }
-}
+});
+
+
