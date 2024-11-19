9675bc9e4f822ba5fbbe6e809a3851bd1d4441ce
Rolly Maulana Awangga <awangga@gmail.com>
2024-11-20 05:46:55 +0700

    ok

--- js/visitor.js
+++ js/visitor.js
@@ -1,5 +1,5 @@
 // Mengimpor FingerprintJS melalui Skypack
-import FingerprintJS from 'https://cdn.skypack.dev/@fingerprintjs/fingerprintjs';
+import FingerprintJS from 'https://cdn.skypack.dev/pin/@fingerprintjs/fingerprintjs@v4.5.1-i3tGtD4H7KJ6bXJV89iM/mode=imports,min/optimized/@fingerprintjs/fingerprintjs.js';
 
 // Memuat FingerprintJS
 FingerprintJS.load()
@@ -7,6 +7,6 @@ FingerprintJS.load()
     .then(result => {
         const visitorId = result.visitorId;
         console.log("Unique Visitor ID:", visitorId);
-        document.body.insertAdjacentHTML('beforeend', `<p>Visitor ID: ${visitorId}</p>`);
+        //document.body.insertAdjacentHTML('beforeend', `<p>Visitor ID: ${visitorId}</p>`);
     })
     .catch(error => console.error("Error loading FingerprintJS:", error));
